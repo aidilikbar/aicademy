2ec2f4ad5228adc9ba55486b9035e64f24a4ee1b
aidil <aidil.ikbar@kpk.go.id>
2025-03-17 15:48:14 +0100

    update header logo

--- resources/views/components/auth-layout.blade.php
+++ resources/views/components/auth-layout.blade.php
@@ -42,7 +42,9 @@
 <body>
     <div class="header d-flex justify-content-between align-items-center px-3">
         <div>
-            <a href="/" class="text-white text-decoration-none">AIcademy</a>
+            <a href="/" class="text-white text-decoration-none">
+                <i class="fas fa-graduation-cap me-2"></i>AIcademy
+            </a>
         </div>
         <div class="form-check form-switch">
             <input class="form-check-input" type="checkbox" id="darkModeToggle">
